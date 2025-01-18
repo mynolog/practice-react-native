9bf650def1e2ea67ce52a05b7b66b16dd41508c9
mynolog <minholee.log@gmail.com>
2025-01-18 16:23:43 +0900

    ✨ feat: 할 일 추가 및 할 일 리스트 구현

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native'
 
 export default function App() {
+  const [goalText, setGoalText] = useState('')
+  const [goals, setGoals] = useState([])
+
+  function handleGoalInput(enteredText) {
+    setGoalText(enteredText)
+  }
+
+  function handleAddGoal() {
+    setGoals((prevState) => [...prevState, goalText])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="할 일을 입력하세요..!"
+          onChangeText={handleGoalInput}
+        />
+        <Button title="Add" onPress={handleAddGoal} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {goals.map((goal, index) => (
+            <View style={styles.goalItem} key={goal + index} r>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: '#fff',
   },
 })
